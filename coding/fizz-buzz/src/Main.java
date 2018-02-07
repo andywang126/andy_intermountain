b679fe2771f3e14b7dca6e534d2eedc8a7a4ccd5
Andy Wang <andywang126@gmail.com>
2018-02-07 15:27:48 -0700

    reformat

--- coding/fizz-buzz/src/Main.java
+++ coding/fizz-buzz/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        for (int i =1; i<=100; i++)
+        for (int i = 1; i <= 100; i++)
             System.out.println(fizzBuzz(i));
     }
 
@@ -9,8 +9,7 @@ public class Main {
     public static String fizzBuzz(int i) {
         if (i % 3 == 0 && i % 5 == 0) {
             return "Fizz Buzz";
-        }
-        else if (i % 3 == 0)
+        } else if (i % 3 == 0)
             return "Fizz";
         else if (i % 5 == 0)
             return "Buzz";
